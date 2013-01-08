eca892183d46d29165c5dd1c9004e13bce1dc655
Adam Tait and Jinjutha Hancock <pair+adam+jinjutha@rally.org>
2013-01-08 15:32:34 -0800

    remove SSL cert verification on other rubies

--- lib/route53.rb
+++ lib/route53.rb
@@ -35,7 +35,7 @@ module Route53
       uri = URI(url)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true if uri.scheme == "https"
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8")
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       time = get_date
       hmac = HMAC::SHA256.new(@secret)
       hmac.update(time)
@@ -92,7 +92,7 @@ module Route53
         uri = URI(@endpoint)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true if uri.scheme == "https"
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE if RUBY_VERSION.start_with?("1.8")
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         resp = nil
         puts "Making Date Request" if @verbose
         http.start { |http| resp = http.head('/date') }
